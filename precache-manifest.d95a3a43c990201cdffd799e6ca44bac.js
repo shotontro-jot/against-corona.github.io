446f247fdf0b68b32466c87ce71e35f9c8ec0d41
r00t <algocerebro@gmail.com>
2020-03-14 23:30:37 +0600

    Updates

---
+++ precache-manifest.d95a3a43c990201cdffd799e6ca44bac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec8e3e05397bbd5a786f42b6583ed3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "373ed2ec7b4dfcfdf95e",
+    "url": "/static/css/main.f884a443.chunk.css"
+  },
+  {
+    "revision": "d3fab9fc4c609e27d516",
+    "url": "/static/js/2.32d434cd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.32d434cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "373ed2ec7b4dfcfdf95e",
+    "url": "/static/js/main.48d15815.chunk.js"
+  },
+  {
+    "revision": "872d3a51183eacc5c5d3",
+    "url": "/static/js/runtime-main.5e20a87a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
